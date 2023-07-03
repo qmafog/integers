6b70f635bffd610f9ff633b307138c94d8206919
qmafog <qmafog6@gmail.com>
2023-07-04 01:19:50 +0300

    done

--- Integers/Consts.cs
+++ Integers/Consts.cs
@@ -4,62 +4,52 @@ namespace Literals
     {
         public static int ReturnLargestInteger()
         {
-            // TODO #3-1. Return the largest possible value of the int type.
-            throw new NotImplementedException();
+            return int.MaxValue;
         }
 
         public static int ReturnSmallestInteger()
         {
-            // TODO #3-2. Return the smallest possible value of the int type.
-            throw new NotImplementedException();
+            return int.MinValue;
         }
 
         public static uint ReturnLargestUnsignedInteger()
         {
-            // TODO #3-3. Return the largest possible value of the uint type.
-            throw new NotImplementedException();
+            return uint.MaxValue;
         }
 
         public static uint ReturnSmallestUnsignedInteger()
         {
-            // TODO #3-4. Return the smallest possible value of the uint type.
-            throw new NotImplementedException();
+            return uint.MinValue;
         }
 
         public static long ReturnLargestLongInteger()
         {
-            // TODO #3-5. Return the largest possible value of the long type.
-            throw new NotImplementedException();
+            return long.MaxValue;
         }
 
         public static long ReturnSmallestLongInteger()
         {
-            // TODO #3-6. Return the smallest possible value of the long type.
-            throw new NotImplementedException();
+            return long.MinValue;
         }
 
         public static ulong ReturnLargestUnsignedLongInteger()
         {
-            // TODO #3-7. Return the largest possible value of the ulong type.
-            throw new NotImplementedException();
+            return ulong.MaxValue;
         }
 
         public static ulong ReturnSmallestUnsignedLongInteger()
         {
-            // TODO #3-8. Return the smallest possible value of the ulong type.
-            throw new NotImplementedException();
+            return ulong.MinValue;
         }
 
         public static short ReturnSmallestShortInteger()
         {
-            // TODO #3-9. Return the smallest possible value of the short type.
-            throw new NotImplementedException();
+            return short.MinValue;
         }
 
         public static short ReturnLargestShortInteger()
         {
-            // TODO #3-10. Return the largest possible value of the short type.
-            throw new NotImplementedException();
+            return short.MaxValue;
         }
     }
 }
--- Integers/LongIntegers.cs
+++ Integers/LongIntegers.cs
@@ -1,77 +1,65 @@
-﻿namespace Literals
+namespace Literals
 {
     public static class LongIntegers
     {
         public static long ReturnLongInteger21()
         {
-            // TODO #2-1. Return "4,956,185,095,298,947,214" decimal literal.
-            throw new NotImplementedException();
+            return 4956185095298947214L;
         }
 
         public static long ReturnLongInteger22()
         {
-            // TODO #2-2. Return "-1,280,010,762,458,239,942" decimal literal.
-            throw new NotImplementedException();
+            return -1280010762458239942L;
         }
 
         public static long ReturnLongInteger23()
         {
-            // TODO #2-3. Return "-945,783,496,234,828,465" decimal literal.
-            throw new NotImplementedException();
+            return -945783496234828465L;
         }
 
         public static ulong ReturnLongInteger24()
         {
-            // TODO #2-4. Return "16,269,823,234,523,742,845" decimal literal.
-            throw new NotImplementedException();
+            return 16269823234523742845UL;
         }
 
         public static long ReturnLongInteger25()
         {
-            // TODO #2-5. Return "9,223,372,036,854,775,807" hexadecimal literal.
-            throw new NotImplementedException();
+            return 0x7FFF_FFFF_FFFF_FFFFL;
         }
 
         public static long ReturnLongInteger26()
         {
-            // TODO #2-6. Return "773,738,404,492,802,748" hexadecimal literal.
-            throw new NotImplementedException();
+            return 0xABCDEFABCDEFABCL;
         }
 
         public static ulong ReturnLongInteger27()
         {
-            // TODO #2-7. Return "17,977,307,477,258,691,517" hexadecimal literal.
-            throw new NotImplementedException();
+            return 0xF97C39E717FA63BDUL;
         }
 
         public static ulong ReturnLongInteger28()
         {
-            // TODO #2-8. Return "14,193,065,825,095,688,383" hexadecimal literal.
-            throw new NotImplementedException();
+            return 0xC4F7E62B7A90A0BFL;
         }
 
         public static long ReturnLongInteger29()
         {
-            // TODO #2-9. Return "4,100,761,908,933,204,629" binary literal.
-            throw new NotImplementedException();
+            return 0b00111000_11101000_11010101_00111010_10111010_01101010_10011010_10010101L;
         }
 
         public static long ReturnLongInteger210()
         {
-            // TODO #2-10. Return "1,645,102,583,813,967,509" binary literal.
-            throw new NotImplementedException();
+            return 0b1011011010100100101000001010101110111010011110000111010010101L;
         }
 
         public static long ReturnLongInteger211()
         {
-            // TODO #2-11. Return "6,148,914,691,236,517,205" binary literal.
-            throw new NotImplementedException();
+            return 0b101010101010101010101010101010101010101010101010101010101010101L;
         }
 
         public static long ReturnLongInteger212()
         {
-            // TODO #2-12. Return "8,446,744,073,709,551,615" binary literal.
-            throw new NotImplementedException();
+            return 0b111010100111000110111001111101101110110000101111111111111111111L;
         }
     }
 }
